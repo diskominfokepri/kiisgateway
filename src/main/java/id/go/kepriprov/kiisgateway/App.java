1b2ad3bbd42f82706e6e241cedd52f14aabd4ce0
mrizkir <rizki@sttindonesia.ac.id>
2018-01-12 22:53:00 +0700

    fix2

--- src/main/java/id/go/kepriprov/kiisgateway/App.java
+++ src/main/java/id/go/kepriprov/kiisgateway/App.java
@@ -1,32 +1,39 @@
 package id.go.kepriprov.kiisgateway;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
-import org.glassfish.jersey.server.ServerProperties;
 import org.glassfish.jersey.servlet.ServletContainer;
 
 import id.go.kepriprov.kiisgateway.lib.Configuration;
 
 public class App {
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) {
 		Configuration config = new Configuration();
 		
 		Server server = new Server(config.getJettyPort());
 		
-		ServletContextHandler context = new ServletContextHandler(server,"/*",ServletContextHandler.SESSIONS);
+		ServletContextHandler ctx = new ServletContextHandler(ServletContextHandler.NO_SESSIONS);
 		
-		ServletHolder sh = new ServletHolder(ServletContainer.class);
-		sh.setInitParameter(ServerProperties.PROVIDER_CLASSNAMES, "id.go.kepriprov.kiisgateway.services.EntryPoint");
-		sh.setInitParameter(ServerProperties.PROVIDER_SCANNING_RECURSIVE, "true");
-        sh.setInitParameter(ServerProperties.TRACING, "ALL");
-        sh.setInitParameter("jersey.config.server.tracing", "ALL");
-        sh.setInitOrder(1);
-		
-        context.addServlet(sh, "/*");
-        server.start();
-        server.join();
+		ctx.setContextPath("/");
+        server.setHandler(ctx);
+        
+        ServletHolder serHol = ctx.addServlet(ServletContainer.class, "/*");
+        serHol.setInitOrder(1);
+        serHol.setInitParameter("jersey.config.server.provider.packages","id.go.kepriprov.kiisgateway.services");
+        try {
+            server.start();
+            server.join();
+        } catch (Exception ex) {
+            Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+
+            server.destroy();
+        }		
 		
 	}
 }
