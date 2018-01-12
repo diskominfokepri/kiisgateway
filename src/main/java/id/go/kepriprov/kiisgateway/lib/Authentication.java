35150d8063ac7d711e14a77e5cc35da067888d44
mrizkir <rizki@sttindonesia.ac.id>
2018-01-12 20:16:44 +0700

    Perbaiki #1

--- src/main/java/id/go/kepriprov/kiisgateway/lib/Authentication.java
+++ src/main/java/id/go/kepriprov/kiisgateway/lib/Authentication.java
@@ -20,19 +20,29 @@ public class Authentication {
 		password2 = (String) Helper.createMD5(password2);
 		password2 = (String) Helper.createSHA1(password2);
 		password2 = (String) Helper.createMD5(password2);
-
 		Database db = new Database();
 		ResultSet hasil = db.query("SELECT * FROM mst_user WHERE id_user = '" + username + "' and password = '" + password2 + "'");
 		String user = "";
 		String pass = "";
+		//String opd = "";
 		if (hasil.next()) {
 			user = hasil.getString("id_user");
 			pass = hasil.getString("password");
+			//opd = 
 		}
 		if (user.equals(username) && pass.equals(password2)) {
 			success = true;
+			//simpan ke log
 		}
-		db.closeConnection();
+		return success;
+	}
+	
+	public boolean isUsernameAndPasswordEmpty(String username, String password) {
+		boolean success = false;		
+		
+		if (username.length() == 0 || password.length() == 0 ) {
+			success = true;
+		}
 		return success;
 	}
 }
--- src/main/java/id/go/kepriprov/kiisgateway/services/EntryPoint.java
+++ src/main/java/id/go/kepriprov/kiisgateway/services/EntryPoint.java
@@ -1,41 +1,59 @@
 package id.go.kepriprov.kiisgateway.services;
 
+
 import javax.ws.rs.GET;
-import javax.ws.rs.HeaderParam;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
 
-
-
 import id.go.kepriprov.kiisgateway.lib.Authentication;
+import id.go.kepriprov.kiisgateway.lib.Database;
 
 
 @Path("/")
 public class EntryPoint {
 
-
 	@GET
 	@Produces(MediaType.TEXT_PLAIN)
-	public String entry() {	
+	public String entry() {
 		return "KIISGateway v.0.1";
 	}
+
 	@GET
 	@Path("/query")
 	@Produces(MediaType.TEXT_PLAIN)
 
-	public String query(@HeaderParam("Username") String username, @HeaderParam("Password") String password,
-			@HeaderParam("REMOTE_ADDR") String ip) throws Exception {
+	// public String query(@HeaderParam("Username") String username,
+	// @HeaderParam("Password") String password) throws Exception {
+	public String query(@Context HttpHeaders httpHeaders) throws Exception {
+		String username = "";
+		String password = "";
+		try {
+			username = httpHeaders.getRequestHeader("Username").get(0);
+			password = httpHeaders.getRequestHeader("Password").get(0);
+			Authentication auth = new Authentication();
+			Database db = new Database();
+			if (auth.isUsernameAndPasswordEmpty(username, password)) {
+				db.insertRecord("INSERT INTO tb_log VALUES (activity,user) VALUES ('Seseorang mencoba untuk login kedalam sistem dengan IP "+ username + "', '" + username + "')");
+				return "usernamekosong";
+			} else {
+				if (auth.checkUsernameAndPassword(username, password)) {
+					return "loginberhasil";
+				} else {
+					String sql = " INSERT INTO tb_log SET id=NULL,activity='User " + username+ " gagal melakukan login karena kesalahan username/password', user='"+ username + "',times=NOW()";
+					db.insertRecord(sql);
+					return "logingagal";
+				}
+				
+			}
 
-		Authentication auth = new Authentication();
-		
-		
-		if (auth.checkUsernameAndPassword(username, password)) {
-			return "sukses";
-		}else {
-			return "gagal";
+		} catch (Exception e) {
+			return "requestgagal";
 		}
-		
+
+				
 	}
-	
+
 }
